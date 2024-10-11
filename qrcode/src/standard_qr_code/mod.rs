51c534d676b553828b2b83dbea8aaa0bbe5e4920
jleicht <jleicht@psi.de>
2024-10-11 10:27:33 +0200

    FIX: limited debugging influence

--- qrcode/src/standard_qr_code/mod.rs
+++ qrcode/src/standard_qr_code/mod.rs
@@ -6,12 +6,12 @@ pub fn qr_code(input: Settings) {
     // all text is assumed to be encoded in byte mode
     if input.debugging {
         println!("test {:?}", input);
-        let (version_no, capacity) = get_verison_info(input.information.len());
-        // calculate width of the code
-        // width = 17 + 4 * <version number>
-        let width: usize = 17 + 4 * version_no;
-        // size of the code with the quiet zone
-        // quiet zone: 4 square whide border around the qr code
-        let width_quite_zone: usize = width + (2 * 8);
     }
+    let (version_no, capacity) = get_verison_info(input.information.len());
+    // calculate width of the code
+    // width = 17 + 4 * <version number>
+    let width: usize = 17 + 4 * version_no;
+    // size of the code with the quiet zone
+    // quiet zone: 4 square whide border around the qr code
+    let width_quite_zone: usize = width + (2 * 8);
 }

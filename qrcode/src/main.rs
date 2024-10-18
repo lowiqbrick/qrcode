808e2557cb5291469f40748b905d2dba47b58f2f
jleicht <jleicht@psi.de>
2024-10-18 14:17:03 +0200

    NEW: added polynomial calculations

--- qrcode/src/main.rs
+++ qrcode/src/main.rs
@@ -1,13 +1,18 @@
 /// this program is supposed to be handed a string which gets converted into
 /// a QR code following norm ISO/IEC CD 18004
 mod input;
+mod polynomials;
 mod standard_qr_code;
 use clap::Parser;
 use input::*;
+use polynomials::test;
 use standard_qr_code::qr_code;
 
 fn main() {
     // get environment variables
     let settings: Settings = Settings::parse();
-    qr_code(settings);
+    qr_code(settings.clone());
+    if settings.debugging {
+        test();
+    }
 }
---
+++ qrcode/src/polynomials/mod.rs
@@ -0,0 +1,180 @@
+use std::fmt::{Display, Formatter, Result};
+use std::ops::{Add, Mul, Sub};
+
+#[derive(Clone, Copy)]
+struct Indeterminate {
+    /// factor in front pof the indeterminate
+    coefficient: i16,
+    /// the 'to the power of' factor
+    degree: u16,
+}
+
+impl Indeterminate {
+    fn new(coefficient: i16, degree: u16) -> Indeterminate {
+        Indeterminate {
+            coefficient,
+            degree,
+        }
+    }
+}
+
+impl Display for Indeterminate {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        // write the individual indeterminate
+        match self.degree {
+            0 => write!(f, "{}", self.coefficient)?,
+            1 => write!(f, "{}x", self.coefficient)?,
+            _ => write!(f, "{}x^{}", self.coefficient, self.degree)?,
+        }
+        write!(f, "{}", "")
+    }
+}
+
+impl Mul for Indeterminate {
+    type Output = Indeterminate;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Indeterminate::new(self.coefficient * rhs.coefficient, self.degree * rhs.degree)
+    }
+}
+
+#[derive(Clone)]
+struct Polynomial {
+    function: Vec<Indeterminate>,
+}
+
+impl Polynomial {
+    fn new(function: Vec<Indeterminate>) -> Polynomial {
+        Polynomial { function }
+    }
+
+    /// method to reduce
+    fn reduce(&mut self) -> Polynomial {
+        // vector to collect all degrees
+        let mut degrees: Vec<u16> = vec![];
+        for x in self.function.iter() {
+            // save local degree
+            let current_degree: u16 = x.degree;
+            // check if current degree is already in degree vector
+            // if not: put it in
+            let mut is_in_already: bool = false;
+            for y in degrees.iter() {
+                if current_degree == *y {
+                    is_in_already = true;
+                }
+            }
+            if !is_in_already {
+                degrees.push(current_degree);
+            }
+        }
+        // take all x'es with the same degrees add their coefficients together
+        // and save them in a new vector
+        let mut result: Vec<Indeterminate> = vec![];
+        for degree in degrees.iter() {
+            let mut degree_coefficient: i16 = 0;
+            for z in self.function.iter() {
+                if *degree == z.degree {
+                    degree_coefficient += z.coefficient;
+                }
+            }
+            result.push(Indeterminate::new(degree_coefficient, *degree));
+        }
+        self.function = result.clone();
+        Polynomial { function: result }
+    }
+}
+
+impl Display for Polynomial {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let mut is_first_element: bool = true;
+        for indeterminate in self.function.iter() {
+            // print the + in front of x
+            // - get printed automatically
+            if indeterminate.coefficient >= 0 && !is_first_element {
+                write!(f, "{}", "+")?;
+            }
+            // write the individual indeterminate
+            match indeterminate.degree {
+                0 => write!(f, "{}", indeterminate.coefficient)?,
+                1 => write!(f, "{}x", indeterminate.coefficient)?,
+                _ => write!(f, "{}x^{}", indeterminate.coefficient, indeterminate.degree)?,
+            }
+            is_first_element = false;
+        }
+        // end
+        write!(f, "{}", "")
+    }
+}
+
+impl Add for Polynomial {
+    type Output = Polynomial;
+    fn add(self, rhs: Self) -> Self::Output {
+        // get current values
+        let mut help_vector: Vec<Indeterminate> = self.function;
+        // add indeterminates from right hand side rhs
+        // adding them to the polynomial
+        for indeterminate in rhs.function.iter() {
+            help_vector.push(*indeterminate);
+        }
+        // return the result
+        // don't forget to .reduce(), which is the "adding" part of this function
+        Polynomial {
+            function: help_vector,
+        }
+        .reduce()
+    }
+}
+
+impl Sub for Polynomial {
+    type Output = Polynomial;
+    fn sub(self, rhs: Self) -> Self::Output {
+        // same as add, but the coefficient gets negated
+        // before it is added, thus subtracting it
+        // get current values
+        let mut help_vector: Vec<Indeterminate> = self.function;
+        // add indeterminates from right hand side rhs
+        // adding them to the polynomial
+        for indeterminate in rhs.function.iter() {
+            let mut intermediate_help: Indeterminate = *indeterminate;
+            intermediate_help.coefficient = indeterminate.coefficient * -1;
+            help_vector.push(intermediate_help);
+        }
+        // return the result
+        // don't forget to .reduce(), which is the "adding" part of this function
+        Polynomial {
+            function: help_vector,
+        }
+        .reduce()
+    }
+}
+
+pub fn test() {
+    // simple test polynomial
+    let test_pol: Polynomial = Polynomial::new(vec![
+        Indeterminate::new(1, 2),
+        Indeterminate::new(2, 1),
+        Indeterminate::new(-4, 0),
+    ]);
+    println!("{}", test_pol);
+    let reduce_test1: Polynomial =
+        Polynomial::new(vec![Indeterminate::new(-3, 4), Indeterminate::new(5, 4)]);
+    println!(
+        "{} simplified to {}",
+        reduce_test1,
+        reduce_test1.clone().reduce()
+    );
+    let add1: Polynomial =
+        Polynomial::new(vec![Indeterminate::new(4, 3), Indeterminate::new(2, 2)]);
+    let add2: Polynomial = Polynomial::new(vec![Indeterminate::new(-2, 3)]);
+    let result_add: Polynomial = add1.clone() + add2.clone();
+    println!("{} plus {} equals {}", add1, add2, result_add);
+    println!(
+        "{} minus {} equals {}",
+        add1,
+        add2,
+        add1.clone() - add2.clone()
+    );
+    let inde1: Indeterminate = Indeterminate::new(3, 2);
+    let inde2: Indeterminate = Indeterminate::new(-1, 6);
+    let result2: Indeterminate = inde1.clone() * inde2.clone();
+    println!("{} multiplied with {} equals {} ", inde1, inde2, result2);
+}

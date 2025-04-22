6d5069953a0968aa88c0f373e7a0850cc39a2698
jleicht <jleicht@psi.de>
2025-04-22 10:44:56 +0200

    FIX: previous galois field generation wrong

--- src/galois_field.rs
+++ src/galois_field.rs
@@ -12,49 +12,42 @@ use crate::polynomials::Indeterminate;
 use super::polynomials::Polynomial;
 
 #[derive(Debug, PartialEq)]
-enum BFBeginning {
-    _Zero,
-    _One,
-    _Other(u8),
-}
-
-#[derive(Debug, PartialEq)]
 pub struct GaloisFields {
     m: u8,
     mod_poly: Polynomial,
-    galois_table: HashMap<u8, BFBeginning>,
+    galois_table: HashMap<u8, u8>,
 }
 
 impl GaloisFields {
-    pub fn new(m: u8, mod_Fx: Polynomial) -> Self {
+    pub fn new(m: u8, mod_fx: Polynomial) -> Self {
         assert!(m > 0);
         assert!(m <= 8);
         let mut res_map = HashMap::new();
 
         // first two entires can be hard coded
-        res_map.insert(0, BFBeginning::_Zero);
-        res_map.insert(1, BFBeginning::_One);
+        res_map.insert(0, 0);
+        res_map.insert(1, 1);
         if m == 1 {
             return GaloisFields {
                 m,
-                mod_poly: mod_Fx,
+                mod_poly: mod_fx,
                 galois_table: res_map,
             };
         }
 
         // insert the simple terms
         let mut last_value = 0;
-        for index in 2..(m + 2) {
+        for index in 2..(m + 1) {
             if (index - 2) < 8 {
-                res_map.insert(index, BFBeginning::_Other(1 << (index - 2)));
-                last_value = 1 << (index - 2);
+                res_map.insert(index, 0b10 << (index - 2));
+                last_value = 0b10 << (index - 2);
             }
         }
 
         // insert remaining values
         // convert polynomial into mask
         let mut mod_mask = 0;
-        for indeter in mod_Fx.get_function() {
+        for indeter in mod_fx.get_function() {
             let current_degree = indeter.get_degree();
             // println!("degree: {}", current_degree);
             assert!((current_degree > -1) && (current_degree < m.into()));
@@ -70,12 +63,12 @@ impl GaloisFields {
         // println!("polynimial as binary: {:8b}", mod_mask);
         // mask values calculate to m
         let upper_limit = match m.cmp(&8) {
-            Ordering::Less => 2_u8.pow(m.into()) + 1,
+            Ordering::Less => 2_u8.pow(m.into()),
             Ordering::Equal => 255_u8,
             Ordering::Greater => panic!("wrong m made it through assert!()"),
         };
         // insert the more complicated parts
-        for index in (m + 2)..upper_limit {
+        for index in (m + 1)..upper_limit {
             let is_msb_set = (last_value & (1 << (m - 1))) > 0;
             let mut current_value = if is_msb_set {
                 ((last_value & 0b0111_1111) << 1) ^ mod_mask
@@ -84,11 +77,11 @@ impl GaloisFields {
             };
             current_value &= limit_mask;
             last_value = current_value;
-            res_map.insert(index, BFBeginning::_Other(current_value));
+            res_map.insert(index, current_value);
         }
         GaloisFields {
             m,
-            mod_poly: mod_Fx,
+            mod_poly: mod_fx,
             galois_table: res_map,
         }
     }
@@ -109,23 +102,17 @@ impl Display for GaloisFields {
                 .galois_table
                 .get_key_value(&key)
                 .expect("key that was upposed to be in hashmap wasn't");
-            match current_enum.1 {
-                BFBeginning::_Zero => write!(f, "{:>8}", "0")?,
-                BFBeginning::_One => write!(f, "{:>8}", "1")?,
-                BFBeginning::_Other(display_vlaue) => {
-                    let mut is_leading = true;
-                    for bit_index in (0..8).rev() {
-                        let mask = 0b0000_0001 << bit_index;
-                        if (display_vlaue & mask) > 0 {
-                            write!(f, "1")?;
-                            is_leading = false;
-                        // only print leading zeroes within the set bit limit m
-                        } else if is_leading && (bit_index >= (self.m)) {
-                            write!(f, " ")?;
-                        } else {
-                            write!(f, "0")?;
-                        }
-                    }
+            let mut is_leading = true;
+            for bit_index in (0..8).rev() {
+                let mask = 0b0000_0001 << bit_index;
+                if (current_enum.1 & mask) > 0 {
+                    write!(f, "1")?;
+                    is_leading = false;
+                // only print leading zeroes within the set bit limit m
+                } else if is_leading && (bit_index >= (self.m)) {
+                    write!(f, " ")?;
+                } else {
+                    write!(f, "0")?;
                 }
             }
             writeln!(f)?;
@@ -144,23 +131,22 @@ fn test_values_m_4() {
     // print value in case of panic!()
     eprintln!("{example}");
     let mut correct_map = HashMap::new();
-    correct_map.insert(0, BFBeginning::_Zero);
-    correct_map.insert(1, BFBeginning::_One);
-    correct_map.insert(2, BFBeginning::_Other(0b0001_u8));
-    correct_map.insert(3, BFBeginning::_Other(0b0010_u8));
-    correct_map.insert(4, BFBeginning::_Other(0b0100_u8));
-    correct_map.insert(5, BFBeginning::_Other(0b1000_u8));
-    correct_map.insert(6, BFBeginning::_Other(0b0011_u8));
-    correct_map.insert(7, BFBeginning::_Other(0b0110_u8));
-    correct_map.insert(8, BFBeginning::_Other(0b1100_u8));
-    correct_map.insert(9, BFBeginning::_Other(0b1011_u8));
-    correct_map.insert(10, BFBeginning::_Other(0b0101_u8));
-    correct_map.insert(11, BFBeginning::_Other(0b1010_u8));
-    correct_map.insert(12, BFBeginning::_Other(0b0111_u8));
-    correct_map.insert(13, BFBeginning::_Other(0b1110_u8));
-    correct_map.insert(14, BFBeginning::_Other(0b1111_u8));
-    correct_map.insert(15, BFBeginning::_Other(0b1101_u8));
-    correct_map.insert(16, BFBeginning::_Other(0b1001_u8));
+    correct_map.insert(0, 0b0000_u8);
+    correct_map.insert(1, 0b0001_u8);
+    correct_map.insert(2, 0b0010_u8);
+    correct_map.insert(3, 0b0100_u8);
+    correct_map.insert(4, 0b1000_u8);
+    correct_map.insert(5, 0b0011_u8);
+    correct_map.insert(6, 0b0110_u8);
+    correct_map.insert(7, 0b1100_u8);
+    correct_map.insert(8, 0b1011_u8);
+    correct_map.insert(9, 0b0101_u8);
+    correct_map.insert(10, 0b1010_u8);
+    correct_map.insert(11, 0b0111_u8);
+    correct_map.insert(12, 0b1110_u8);
+    correct_map.insert(13, 0b1111_u8);
+    correct_map.insert(14, 0b1101_u8);
+    correct_map.insert(15, 0b1001_u8);
     let correct_result = GaloisFields {
         m: 4,
         mod_poly: Polynomial::new(vec![Indeterminate::new(1, 1), Indeterminate::new(1, 0)]),

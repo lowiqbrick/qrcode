2dcad9410e8dd64c208eff12fa38021dd9cc99bd
jleicht <jleicht@psi.de>
2025-06-20 17:04:53 +0200

    NEW: reed solomon calculation #4
    Understanding and implementing this took way to long.

--- src/galois_field.rs
+++ src/galois_field.rs
@@ -1,7 +1,7 @@
 /// based on "Tutorial on Reed-Solomon Error Correction Coding" by William A. Geisel (August 1990)
 /// https://ntrs.nasa.gov/api/citations/19900019023/downloads/19900019023.pdf
 /// (last viewed 17.04.2025)
-use std::{collections::HashMap, fmt::Display};
+use std::{collections::HashMap, fmt::Display, vec};
 
 use crate::polynomials::Indeterminate;
 
@@ -90,9 +90,10 @@ impl GaloisFields {
         }
     }
 
-    pub fn _get_alpha(&self, alpha_indice: u8) -> u8 {
+    /// takes the index of an alpha value and returns the value stored in that alpha
+    pub fn index_to_value(&self, alpha_indice: u8) -> u8 {
         // compensate the frost two elements 0 and 1 in the galois field
-        let indice = alpha_indice + 2;
+        let indice = ((alpha_indice as u16 + 1_u16) % 256) as u8;
         assert!(self.galois_table.len() >= (indice as usize));
         // get value
         if let Some(alpha) = self.galois_table.get(&indice) {
@@ -101,40 +102,160 @@ impl GaloisFields {
             panic!("key {} wasn't in hashmap {:?}", indice, self.galois_table);
         }
     }
+
+    /// takes an alpha value and returns the index that value has in the galois field
+    pub fn value_to_index(&self, value: u8) -> Result<u8, String> {
+        for (index, value_map) in self.galois_table.clone().into_iter() {
+            if value_map == value && index != 0 {
+                return Ok(index - 1);
+            }
+        }
+        Err(format!("value {} not found", value))
+    }
+
     /// meant to be used on a galosi field of m=8 and Polynomial x^4+x^3+x^2+1
     pub fn _correction_polynomial(&self, num_error_corr: u8) -> Option<Polynomial> {
         match num_error_corr {
             7 => Some(Polynomial::new(vec![
                 Indeterminate::new(1, 7),
-                Indeterminate::new(self._get_alpha(87) as i8, 6),
-                Indeterminate::new(self._get_alpha(229) as i8, 5),
-                Indeterminate::new(self._get_alpha(146) as i8, 4),
-                Indeterminate::new(self._get_alpha(149) as i8, 3),
-                Indeterminate::new(self._get_alpha(238) as i8, 2),
-                Indeterminate::new(self._get_alpha(102) as i8, 1),
-                Indeterminate::new(self._get_alpha(21) as i8, 0),
+                Indeterminate::new(self.index_to_value(87), 6),
+                Indeterminate::new(self.index_to_value(229), 5),
+                Indeterminate::new(self.index_to_value(146), 4),
+                Indeterminate::new(self.index_to_value(149), 3),
+                Indeterminate::new(self.index_to_value(238), 2),
+                Indeterminate::new(self.index_to_value(102), 1),
+                Indeterminate::new(self.index_to_value(21), 0),
+            ])),
+            10 => Some(Polynomial::new(vec![
+                Indeterminate::new(1, 10),
+                Indeterminate::new(self.index_to_value(251), 9),
+                Indeterminate::new(self.index_to_value(67), 8),
+                Indeterminate::new(self.index_to_value(46), 7),
+                Indeterminate::new(self.index_to_value(61), 6),
+                Indeterminate::new(self.index_to_value(118), 5),
+                Indeterminate::new(self.index_to_value(70), 4),
+                Indeterminate::new(self.index_to_value(64), 3),
+                Indeterminate::new(self.index_to_value(94), 2),
+                Indeterminate::new(self.index_to_value(32), 1),
+                Indeterminate::new(self.index_to_value(45), 0),
             ])),
             16 => Some(Polynomial::new(vec![
                 Indeterminate::new(1, 16),
-                Indeterminate::new(self._get_alpha(120) as i8, 15),
-                Indeterminate::new(self._get_alpha(104) as i8, 14),
-                Indeterminate::new(self._get_alpha(107) as i8, 13),
-                Indeterminate::new(self._get_alpha(109) as i8, 12),
-                Indeterminate::new(self._get_alpha(102) as i8, 11),
-                Indeterminate::new(self._get_alpha(161) as i8, 10),
-                Indeterminate::new(self._get_alpha(76) as i8, 9),
-                Indeterminate::new(self._get_alpha(3) as i8, 8),
-                Indeterminate::new(self._get_alpha(91) as i8, 7),
-                Indeterminate::new(self._get_alpha(191) as i8, 6),
-                Indeterminate::new(self._get_alpha(147) as i8, 5),
-                Indeterminate::new(self._get_alpha(169) as i8, 4),
-                Indeterminate::new(self._get_alpha(182) as i8, 3),
-                Indeterminate::new(self._get_alpha(194) as i8, 2),
-                Indeterminate::new(self._get_alpha(225) as i8, 1),
-                Indeterminate::new(self._get_alpha(120) as i8, 0),
+                Indeterminate::new(self.index_to_value(120), 15),
+                Indeterminate::new(self.index_to_value(104), 14),
+                Indeterminate::new(self.index_to_value(107), 13),
+                Indeterminate::new(self.index_to_value(109), 12),
+                Indeterminate::new(self.index_to_value(102), 11),
+                Indeterminate::new(self.index_to_value(161), 10),
+                Indeterminate::new(self.index_to_value(76), 9),
+                Indeterminate::new(self.index_to_value(3), 8),
+                Indeterminate::new(self.index_to_value(91), 7),
+                Indeterminate::new(self.index_to_value(191), 6),
+                Indeterminate::new(self.index_to_value(147), 5),
+                Indeterminate::new(self.index_to_value(169), 4),
+                Indeterminate::new(self.index_to_value(182), 3),
+                Indeterminate::new(self.index_to_value(194), 2),
+                Indeterminate::new(self.index_to_value(225), 1),
+                Indeterminate::new(self.index_to_value(120), 0),
             ])),
             _ => None,
         }
+    }
+
+    pub fn calculate_error_correction(
+        data_bytes: Polynomial,
+        correction_polynomial: Polynomial,
+        galois_field: GaloisFields,
+    ) -> Polynomial {
+        println!("given data: {}", data_bytes);
+        let mut work_data_bytes = data_bytes.clone();
+        let number_iterations =
+            data_bytes.get_function().len() - correction_polynomial.get_function().len();
+        let num_valid_operations = correction_polynomial.get_function().len();
+        println!("num valid operations: {}", num_valid_operations);
+        println!("number itertaions: {}", number_iterations);
+        for iteration_index in 0..=number_iterations {
+            // get highest degree
+            let mut highest_degree = 0;
+            for element in work_data_bytes.get_function().iter() {
+                if element.get_degree() > highest_degree && element.get_coefficient() != 0 {
+                    highest_degree = element.get_degree();
+                }
+            }
+            // println!("highest degree: {}", highest_degree);
+            // get value of highest degree
+            let mut highest_degree_value_option = None;
+            for element in work_data_bytes.get_function().iter() {
+                if element.get_degree() == highest_degree {
+                    highest_degree_value_option = Some(element.get_coefficient());
+                }
+            }
+            if highest_degree_value_option.is_none() {
+                panic!("highest degree couldn't be found even though is should exitst");
+            }
+            let highest_degree_value = highest_degree_value_option.unwrap();
+            println!("highest degree value: {}", highest_degree_value);
+            // multiply everything with highest value
+            let mut offset = iteration_index;
+            let mut working_iterations = num_valid_operations;
+            // is the same for all following iterations
+            let highest_value_index = galois_field.value_to_index(highest_degree_value).unwrap();
+            println!(
+                "index of the highest value in the data is {}",
+                highest_value_index
+            );
+            for (index, element) in work_data_bytes.get_function_mut().iter_mut().enumerate() {
+                if offset == 0 && working_iterations != 0 {
+                    // get alpha indices from correction and highest value
+                    let mut value_correction_value = None;
+                    for correction_element in correction_polynomial.clone() {
+                        if correction_element.get_degree()
+                            == (working_iterations - 1).try_into().unwrap()
+                        {
+                            value_correction_value = Some(correction_element.get_coefficient());
+                        }
+                    }
+                    let value_correction_index = if value_correction_value.is_some() {
+                        galois_field
+                            .value_to_index(value_correction_value.unwrap())
+                            .unwrap()
+                    } else {
+                        panic!(
+                            "no index for value {} in galois field",
+                            value_correction_value.unwrap()
+                        );
+                    };
+                    println!("--------------\ndata byte at index {}", index);
+                    // add indices
+                    let new_alpha_index =
+                        ((highest_value_index as u16 + value_correction_index as u16) % 255) as u8;
+                    println!(
+                        "added indices ({} + {}) % 255 = {}",
+                        highest_value_index, value_correction_index, new_alpha_index
+                    );
+                    let new_alpha_value = galois_field.index_to_value(new_alpha_index);
+                    // XOR the values
+                    let final_value = new_alpha_value ^ element.get_coefficient();
+                    println!(
+                        "XOR'ing {} (index {}) and {} to {}",
+                        new_alpha_value,
+                        new_alpha_index,
+                        element.get_coefficient(),
+                        final_value
+                    );
+                    // assign new value
+                    element.set_coefficient(final_value);
+                }
+                if working_iterations != 0 && offset == 0 {
+                    working_iterations -= 1;
+                }
+                if offset != 0 {
+                    offset -= 1;
+                }
+            }
+            println!("after iteration {}: {}", iteration_index, work_data_bytes);
+        }
+        work_data_bytes
     }
 }
 
@@ -188,7 +309,6 @@ impl Display for GaloisFields {
     }
 }
 
-// TODO remove debugging test
 #[test]
 fn test_values_m_4() {
     let example = GaloisFields::_new(
@@ -220,4 +340,97 @@ fn test_values_m_4() {
         galois_table: correct_map,
     };
     assert_eq!(example, correct_result);
+}
+
+#[test]
+fn sanity_alpha_conversion() {
+    let galois_field = GaloisFields::_new(
+        8,
+        Polynomial::new(vec![
+            Indeterminate::new(1, 4),
+            Indeterminate::new(1, 3),
+            Indeterminate::new(1, 2),
+            Indeterminate::new(1, 0),
+        ]),
+    );
+    let alpha_index = 120;
+    let alpha_value = galois_field.index_to_value(alpha_index);
+    let reconstructed_alpha_index = galois_field.value_to_index(alpha_value).unwrap();
+    assert_eq!(alpha_index, reconstructed_alpha_index);
+}
+
+#[test]
+/// default example on red titan
+/// https://www.pclviewer.com/rs2/calculator.html (last viewed 20.06.2025)
+fn test_calculate_error_correction() {
+    let galois_field = GaloisFields::_new(
+        8,
+        Polynomial::new(vec![
+            Indeterminate::new(1, 4),
+            Indeterminate::new(1, 3),
+            Indeterminate::new(1, 2),
+            Indeterminate::new(1, 0),
+        ]),
+    );
+    let Some(correction_polynomial) = galois_field._correction_polynomial(10) else {
+        panic!("test couldn't obtain generator polynomial for 10 error bytes");
+    };
+    let mut data = Polynomial::new(vec![
+        Indeterminate::new(16, 15),
+        Indeterminate::new(32, 14),
+        Indeterminate::new(12, 13),
+        Indeterminate::new(86, 12),
+        Indeterminate::new(97, 11),
+        Indeterminate::new(128, 10),
+        Indeterminate::new(236, 9),
+        Indeterminate::new(17, 8),
+        Indeterminate::new(236, 7),
+        Indeterminate::new(17, 6),
+        Indeterminate::new(236, 5),
+        Indeterminate::new(17, 4),
+        Indeterminate::new(236, 3),
+        Indeterminate::new(17, 2),
+        Indeterminate::new(236, 1),
+        Indeterminate::new(17, 0),
+    ]);
+    data = data * Polynomial::new(vec![Indeterminate::new(1, 10)]);
+    // append missing 0x^n's
+    for index in (0..=9).rev() {
+        data.get_function_mut().push(Indeterminate::new(0, index));
+    }
+    let result =
+        GaloisFields::calculate_error_correction(data, correction_polynomial, galois_field);
+    let expected_result = Polynomial::new(vec![
+        Indeterminate::new(0, 25),
+        Indeterminate::new(0, 24),
+        Indeterminate::new(0, 23),
+        Indeterminate::new(0, 22),
+        Indeterminate::new(0, 21),
+        Indeterminate::new(0, 20),
+        Indeterminate::new(0, 19),
+        Indeterminate::new(0, 18),
+        Indeterminate::new(0, 17),
+        Indeterminate::new(0, 16),
+        Indeterminate::new(0, 15),
+        Indeterminate::new(0, 14),
+        Indeterminate::new(0, 13),
+        Indeterminate::new(0, 12),
+        Indeterminate::new(0, 11),
+        Indeterminate::new(0, 10),
+        Indeterminate::new(165, 9),
+        Indeterminate::new(36, 8),
+        Indeterminate::new(212, 7),
+        Indeterminate::new(193, 6),
+        Indeterminate::new(237, 5),
+        Indeterminate::new(54, 4),
+        Indeterminate::new(199, 3),
+        Indeterminate::new(135, 2),
+        Indeterminate::new(44, 1),
+        Indeterminate::new(85, 0),
+    ]);
+    println!(
+        "results\ncalculated: {}\nexpected:   {}",
+        result, expected_result
+    );
+    assert_eq!(result, expected_result);
 }
